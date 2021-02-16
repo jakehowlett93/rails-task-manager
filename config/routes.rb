3e44e3b8faa181857b2952a34a9bc7e14a16b68c
jake howlett <jake.howlett93@gmail.com>
2021-02-16 13:18:53 +0000

    added route and action for the update method

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/tasks/:id', to: 'tasks#show', as: :task
   post '/tasks', to: 'tasks#create'
   get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
 end
